9ab4f413cb190fbf8cbc42ce656b45057fafc4db
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-12 06:25:38 +0000

    Recursive function to calculate sum from arguments a to b

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  // fromN is always < toN
+
+  // base case is when fromN === toN
+
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
+
+};
 
 module.exports = sum;
+
+console.log(sum(3, 10));
